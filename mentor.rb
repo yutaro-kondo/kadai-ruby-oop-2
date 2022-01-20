1c104469f5807b10962139511d5bb4c3aee39cd6
yutaro,kondo <yutty09051@gmail.com>
2022-01-20 12:06:34 +0000

    kadai

---
+++ mentor.rb
@@ -0,0 +1,34 @@
+class Mentor
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    
+    @name = name
+    
+  end
+  
+  def job
+    
+    puts "#{@name}です。私は現役のITプロフェッショナルです。"
+  
+  end
+
+end
+
+class RailsMentor < Mentor
+  
+  def job
+    
+    puts "#{@name}です。私は現役のRubyとRailsでWebアプリケーションを作ります。"
+  
+  end
+  
+end
+
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
+
+kirameki.job
+akaide.job
+
